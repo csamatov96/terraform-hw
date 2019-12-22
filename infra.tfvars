fe1b6293e024ab911bad9c937a62744e56977381
csamatov96 <csamatov96@gmail.com>
2019-12-22 17:26:49 -0600

    changes

---
+++ infra.tfvars
@@ -0,0 +1,1 @@
+private_ip = "~/.ssh/id_rsa"
--- wordpress.tf
+++ wordpress.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "wordpress" { 
-  count = 3
-  ami             = "ami-4bf3d731"
+  count = 1
+  ami             = "ami-4bf3d731" #centos
   instance_type   = "t2.micro"
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
   security_groups = ["sg"] #refer only by its name not resource name
-  user_data = file("userdata_file")
-  
+  #user_data = file("userdata_file")
+
+provisioner "local-exec" { #it'll download locally 
+    command = ["sudo echo ${aws_instance.wordpress.private_ip} >> private_ips.txt"
+	 			"sudo wget -O https://wordpress.org/latest.tar.gz"
+	 			"sudo mkdir /tmp"] 
+}
+
 
 }
