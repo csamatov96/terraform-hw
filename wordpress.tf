543eba6ae67aff354d8dc4f5aacc9912dd7a490f
csamatov96 <csamatov96@gmail.com>
2019-12-22 17:47:50 -0600

    userdata

--- wordpress.tf
+++ wordpress.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "wordpress" {
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
   security_groups = ["sg"] #refer only by its name not resource name
-  #user_data = file("userdata_file")
+  user_data = file("userdata_file")
 
 provisioner "local-exec" { #it'll download locally 
     command = "echo ${aws_instance.wordpress.private_ip} >> private_ips.txt"
