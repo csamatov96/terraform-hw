867ed7a52ef32b942c65eb9505d01942e06769cf
csamatov96 <csamatov96@gmail.com>
2019-12-23 14:45:45 -0600

    cahnges

---
+++ task2/subnet_association.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public1.id
+  subnet_id      = aws_subnet.public2.id
+  subnet_id      = aws_subnet.public3.id
+
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_route_table_association" "b" {
+  gateway_id     = aws_internet_gateway.gw.id
+  route_table_id = aws_route_table.r.id
+}
