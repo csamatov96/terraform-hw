842402006164a0acba40dd37d558b88b091879e6
csamatov96 <csamatov96@gmail.com>
2019-12-23 15:13:11 -0600

    chang

--- task2/test.tf
+++ task2/test.tf
@@ -2,7 +2,7 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public1.id 
   route_table_id = aws_route_table.r.id
 }
-resource "aws_route_table_association" "b" {
-  gateway_id     = aws_internet_gateway.gw.id
-  route_table_id = aws_route_table.r.id
-}
+#resource "aws_route_table_association" "b" {
+#  gateway_id     = aws_internet_gateway.gw.id
+#  route_table_id = aws_route_table.r.id
+#}
