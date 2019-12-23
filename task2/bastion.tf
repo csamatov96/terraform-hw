ff438120cf9b10293cc7079d44aac1c7032296e6
csamatov96 <csamatov96@gmail.com>
2019-12-23 16:51:10 -0600

    sec_group

--- task2/bastion.tf
+++ task2/bastion.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
 #  security_groups = ["allow_ssh_http"] #refer only by its name not resource name
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh_http.id}"]
+  vpc_security_group_ids = ["${aws_security_group.sec_group.id}"]
 
   lifecycle{
     prevent_destroy = false
