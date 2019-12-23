9e2798540738fca09a0fed36e992d0cda9939b1d
csamatov96 <csamatov96@gmail.com>
2019-12-23 16:21:12 -0600

    changes

--- task2/bastion.tf
+++ task2/bastion.tf
@@ -12,22 +12,4 @@ resource "aws_key_pair" "key_resource" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_security_group" "sec_group" {
-  name        = "allow_ssh" #refer to it by its name 
-  description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-028708f18eb20462c" #ID of created VPC
-
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
 }
