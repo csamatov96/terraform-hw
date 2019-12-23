3a8dbf2786219ce977645f6a19269685350f82cd
csamatov96 <csamatov96@gmail.com>
2019-12-23 15:35:55 -0600

    changes

--- task2/private_association.tf
+++ task2/private_association.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table_association" "d" {
   subnet_id      = aws_subnet.private1.id 
-  route_table_id = aws_route_table.r.id
+  route_table_id = aws_route_table.r2.id
 }
 resource "aws_route_table_association" "e" {
   subnet_id      = aws_subnet.private2.id 
-  route_table_id = aws_route_table.r.id
+  route_table_id = aws_route_table.r2.id
 }
 resource "aws_route_table_association" "f" {
   subnet_id      = aws_subnet.private3.id 
-  route_table_id = aws_route_table.r.id
+  route_table_id = aws_route_table.r2.id
 }
