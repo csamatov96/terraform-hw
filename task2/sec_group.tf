6f041c21e4025422ae4e9b7324f30212bb74ff81
csamatov96 <csamatov96@gmail.com>
2019-12-23 16:24:54 -0600

    changes

--- task2/sec_group.tf
+++ task2/sec_group.tf
@@ -3,17 +3,18 @@ resource "aws_security_group" "sec_group" {
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-028708f18eb20462c" #ID of created VPC
 
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
+
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+
 }
