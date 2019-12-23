6b003e6114edea12ddbe31b7a6fbfd3facc884bf
csamatov96 <csamatov96@gmail.com>
2019-12-23 14:46:33 -0600

    changes

--- task2/subnet_association.tf
+++ task2/subnet_association.tf
@@ -1,7 +1,6 @@
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public1.id
-  subnet_id      = aws_subnet.public2.id
-  subnet_id      = aws_subnet.public3.id
+ 
 
   route_table_id = aws_route_table.r.id
 }
