ec8e5e1c8e4fa905a667c6ad774fc82f152a60a6
csamatov96 <csamatov96@gmail.com>
2019-12-23 15:27:11 -0600

    changes

--- task2/nat-gw.tf
+++ task2/nat-gw.tf
@@ -1,5 +1,6 @@
 data "aws_nat_gateway" "ngw" {
-  subnet_id = "${aws_subnet.private1.id}"
+    allocation_id = "${aws_eip.nat.id}"
+    subnet_id = "${aws_subnet.private1.id}"
 
   tags = {
     Name = "gw NAT"
