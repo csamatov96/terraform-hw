f440396a653f983f81b54ec71ad6247d63fbf2b6
csamatov96 <csamatov96@gmail.com>
2019-12-23 16:07:14 -0600

    subnet_id     = "${aws_subnet.public.id}"

--- task2/nat-gw.tf
+++ task2/nat-gw.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "tfeip"{
 # Create NAT Gateway
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = "${aws_eip.tfeip.id}"
-  subnet_id  = "${element(data.aws_subnet_ids.public1.ids, 0)}"
+  subnet_id     = "${aws_subnet.public1.id}"
   depends_on = ["aws_internet_gateway.gw"]
 
 }
