5b6638c8ee96f884d1403aaed37281b01b1f80e2
csamatov96 <csamatov96@gmail.com>
2019-12-23 15:04:39 -0600

    changes

--- task2/test.tf
+++ task2/test.tf
@@ -1,5 +1,5 @@
 resource "aws_main_route_table_association" "a" {
-  vpc_id         = "${aws_vpc.main.id}"
+  #vpc_id         = "${aws_vpc.main.id}"
   subnet_id = ["${aws_subnet.public1}","${aws_subnet.public2}","${aws_subnet.public3}"]
   
   route_table_id = "${aws_route_table.r.id}"
