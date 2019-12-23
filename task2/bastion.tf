d08853a86c2c7d4506e8df636f96dad7d8514e13
csamatov96 <csamatov96@gmail.com>
2019-12-23 17:26:22 -0600

    changes

--- task2/bastion.tf
+++ task2/bastion.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "web" {
   associate_public_ip_address = "true"
   key_name = aws_key_pair.key_resource.key_name
 #  security_groups = ["allow_ssh_http"] #refer only by its name not resource name
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id = var.subnet_id
+  vpc_security_group_ids = "${var.vpc_security_group_ids}"
+  subnet_id = "${var.subnet_id}"
 
   lifecycle{
     prevent_destroy = false
